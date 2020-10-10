75cac33ca4205c6c52fbec96caee17475e95664d
Kadir Can KARADEMİR <50851192+CanKadirKarademir@users.noreply.github.com>
2020-10-10 15:20:55 +0300

    feat: added client header and banner components

--- src/app/components/layouts/client/index.ts
+++ src/app/components/layouts/client/index.ts
@@ -1,1 +1,3 @@
-export * from './client-layout/client-layout.component'
+export * from './client-layout/client-layout.component';
+export * from './client-banner/client-banner.component';
+export * from './client-header/client-header.component';
